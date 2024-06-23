0a5c5a3c5359e54472e7e09c26a89529f60d4210
Tori Rodgers <torirodgers22@gmail.com>
2024-06-22 20:57:40 -0500

    Chapter 7 and 8 Studio

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -5,42 +5,62 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
+// let check = protoArray1.includes(",");
+// console.log(check);
+
+
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
-	let output;
-	//TODO: 2. write the code required for this step
+	let check = protoArray1.includes(",");
+	console.log(check);
 
+	//TODO: 2. write the code required for this step
+	let output2 = protoArray1.split(",");
+	output2.reverse();
+	let output = output2.join(',');
+
+	
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
 
 //3)
 function semiDash() {
-	let check;
-	let output;
+	let check = protoArray2.includes(";");
+	console.log(check);
+	
 //TODO: write the code required for this step
-
+	let newArray = protoArray2.split(";");
+	newArray.sort();
+	let output = newArray.join('-');
   
 	return output;
 }
 
 //4)
 function reverseSpaces() {
-	let check;
-	let output;
+	let check = protoArray3.includes(" ");
+	console.log(check);
+	
   //TODO: write the code required for this step
+	let newArray = protoArray3.split(" ");
+	newArray.sort();
+	newArray.reverse();
+	let output = newArray.join(" ");
 
 	return output;
 }
 
 //5)
 function commaSpace() {
-	let check;
-	let output;
+	let check = protoArray4.includes(", ");
+	console.log(check);
+	
 	//TODO: write the code required for this step
-  
+	let newArray = protoArray4.split(" ");
+	let output = newArray.join("");
+
 	return output;
 }
 
