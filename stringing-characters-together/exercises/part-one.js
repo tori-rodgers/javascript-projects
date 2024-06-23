fcc2a34b11ba3e83431c3bc4b7afa5616ea2c4be
Tori Rodgers <torirodgers22@gmail.com>
2024-06-22 22:52:27 -0500

    Part 1,2,3 of Strings Exercises

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -4,7 +4,18 @@ let num = 1001;
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+console.log(String(num));
+console.log(typeof (String(num)));
 
+console.log(num.toString().length)
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+let num2 = 3.14
+console.log(String(num2));
+console.log(typeof (String(num2)));
+console.log(num.toString().length);
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+if (String(num2).includes(".")){
+    console.log(String(num2).length-1);
+} else {
+    console.log(String(num2).length); 
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,14 +4,23 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+// console.log(language.slice(0,1)+language.slice(4,5));
 //2. Without using slice(), use method chaining to accomplish the same thing.
+let newString = language.replace('JavaScript', ('JS'));
+console.log(newString);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+let initials = 'JS';
+console.log(`The abbreviation for '${language}' is '${initials}'.`)
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+let str = "  awww, what a cute cat"
+console.log(`${(str.trim()).replace(str[2], str[2].toUpperCase())}!`);
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+console.log(notTitleCase.indexOf('t'));
+console.log(notTitleCase.indexOf('c'));
+console.log((notTitleCase.replace("t", "T")).replace("c", "C"));
