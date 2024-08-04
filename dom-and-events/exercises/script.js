028f45450ca0e742addcf209a9338aff19fd7c2e
Tori Rodgers <torirodgers22@gmail.com>
2024-08-03 20:51:50 -0500

    completed exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,23 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', function (event) {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+    missionAbort.addEventListener('mouseover', function (event) {
+        event.target.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+     missionAbort.addEventListener('click', function () {
+        let response = confirm("\nAre you sure you want to abort the mission?");
+        if (response === true) {
+            paragraph.innerHTML = 'Mission Aborted! Space shuttle returning home';
+        };
+
+     });
+
 }
 
 window.addEventListener("load", init);
